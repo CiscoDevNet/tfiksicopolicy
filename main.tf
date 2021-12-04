696d92d32829b9da4eac83133a02adb1738db159
prathjan <prathjan@cisco.com>
2021-12-04 13:41:02 -0800

    name change

--- main.tf
+++ main.tf
@@ -91,7 +91,8 @@ module "network" {
 module "version_1-19-15-iks3" {
   source           = "terraform-cisco-modules/iks/intersight//modules/version"
   version = "=2.1.2"
-  policyName     = "1.19.15-iks.3"
+  policyName     = local.k8s_version_name
+  # policyName     = "1.19.15-iks.3"
   iksVersionName = "1.19.15-iks.3"
   org_name = local.organization
 #  tags     = var.tags
